a257826e27cd5688239b4ec91b041a7ed72d31d0
Your Name <you@example.com>
2021-03-18 17:25:49 +0530

    move to git

---
+++ target/classes/sd/main.js
@@ -0,0 +1,6 @@
+/* global vertx */
+/* global console */
+
+// When Vert.x creates a new verticle, the "vertx" instance is already part of the scope, so we do not need to declare it.
+
+console.log("Main verticle deployed.");
